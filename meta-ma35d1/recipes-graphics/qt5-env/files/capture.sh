ecd950152e93aa24f4a0b2f419fbadacc5361311
Wayne <wosayttn@gmail.com>
2021-05-28 21:02:11 +0800

    Delete meta-ma35d1/recipes-graphics/qt5-env directory

--- meta-ma35d1/recipes-graphics/qt5-env/files/capture.sh
+++
@@ -1,1 +0,0 @@
-gst-launch-1.0 v4l2src num-buffers=1 device=/dev/video0 ! jpegenc ! filesink location=$1
--- meta-ma35d1/recipes-graphics/qt5-env/files/qt5-env.sh
+++
@@ -1,6 +0,0 @@
-
-#define a new path for the font in qt5
-export QT_QPA_FONTDIR=/usr/share/fonts/truetype
-export XDG_RUNTIME_DIR="/tmp/runtime-root"
-export QT_QPA_PLATFORM="linuxfb"
-
